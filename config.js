c0301ecf7109c54ee2bc86bf7c9c15a877534496
DUCTHUAN279 <101004217+DUCTHUAN55@users.noreply.github.com>
2022-03-07 02:24:28 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "ducthuanchucmung8thang3",
     introTitle: 'hi chào ánh tuyết',
-    introDesc: `bạn có một tin nhắn từ đức thuận `,
+    introDesc: `bạn có một tin nhắn từ DUC THUAN `,
     btnIntro: '^^HiHi^^',
     title: 'Ngày 8-3 chúc một nửa thế giới luôn thành công trong cuộc sống 🥰',
     desc: 'Chúc bạn luôn duyên dáng và xinh đẹp trong mắt một nửa thế giới còn lại. ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Chấm chấm mút mút đút vào lỗ trôn. Là hành động gì <3',
+    question: 'Cái gì đến với chị em phụ nữ hàng tháng? <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: ' Xâu kim  <33333333',
+    reply: ' Tạp chí phụ nữ hàng tháng<333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'thanks bạn đã xem.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Happy Women’s day..',
+    btnAccept: 'Okiii lun <3',
     messLink: 'https://github.com/DUCTHUAN55/Confess-Crush' //link mess của các bạn.
 }
